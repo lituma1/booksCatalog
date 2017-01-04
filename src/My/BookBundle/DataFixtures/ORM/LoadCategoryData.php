3b0ead57871515f87df0c6b743430222d9033c09
Piotr Popkiewicz <lituma1@gmail.com>
2017-01-04 21:47:14 +0100

    Improve LoadCategoryData file

--- src/My/BookBundle/DataFixtures/ORM/LoadCategoryData.php
+++ src/My/BookBundle/DataFixtures/ORM/LoadCategoryData.php
@@ -11,6 +11,43 @@
  *
  * @author pp
  */
-class LoadCategoryData {
-    //put your code here
+namespace MyBookBundle\DataFixtures\ORM;
+
+use \Doctrine\Common\DataFixtures\AbstractFixture;
+use \Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use \Doctrine\Common\Persistence\ObjectManager;
+use My\BookBundle\Entity\Category;
+
+class LoadCategoryData extends AbstractFixture implements OrderedFixtureInterface{
+    
+    public function getOrder() {
+        return 1;
+    }
+
+    public function load(ObjectManager $manager) {
+        $category1 = new Category();
+        $category1->setName('sci-fi');
+        $this->addReference('sci-fiCategory', $category1);
+        
+        $category2 = new Category();
+        $category2->setName('fantasy');
+        $this->addReference('fantasyCategory', $category2);
+        
+        $category3 = new Category();
+        $category3->setName('horror');
+        $this->addReference('horrorCategory', $category3);
+        
+        $category4 = new Category();
+        $category4->setName('dramat');
+        $this->addReference('dramatCategory', $category4);
+        
+        
+        $manager->persist($category1);
+        $manager->persist($category2);
+        $manager->persist($category3);
+        $manager->persist($category4);
+        
+        $manager->flush();
+    }
+
 }
